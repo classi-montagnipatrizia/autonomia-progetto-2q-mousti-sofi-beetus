c79fea3bdb2134e100836b8c71d057bf4fffe6b0
Moustapha Mbaye <moustapha.mbaye@marconirovereto.it>
2026-04-13 19:09:22 +0200

    feat: aggiornato il layout principale con padding ottimizzato e inclusione del drawer utenti online

--- frontend/src/app/layout/main-layout/main-layout-component/main-layout-component.ts
+++ frontend/src/app/layout/main-layout/main-layout-component/main-layout-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../header/header-component/header-component';
+import { OnlineUsersDrawerComponent } from '../../../features/home/components/online-users-drawer/online-users-drawer-component/online-users-drawer-component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, OnlineUsersDrawerComponent],
   templateUrl: './main-layout-component.html',
   styleUrl: './main-layout-component.scss',
 })
