9925cfafe370d8e587c61d0c0f6e238ad364d53a
Moustapha Mbaye <moustapha.mbaye@marconirovereto.it>
2026-04-14 09:41:59 +0200

    fix: aggiornato gli URL API e WebSocket per l'ambiente di produzione

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'http://localhost:8080/api',
-  wsUrl: 'http://localhost:8080/ws',
+  apiUrl: 'https://unexpected-julie-recognised-legislation.trycloudflare.com/api',
+  wsUrl: 'https://unexpected-julie-recognised-legislation.trycloudflare.com/ws',
 
   // Cloudinary Storage Configuration
   cloudinary: {
