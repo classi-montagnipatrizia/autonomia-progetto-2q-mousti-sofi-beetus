305dbc32790ad35a6939e3554fbbc9f0d25e573f
Moustapha Mbaye <moustapha.mbaye@marconirovereto.it>
2026-04-28 09:10:09 +0200

    fix: abilita la serializzazione delle pagine nel servizio backend

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -4,10 +4,12 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BackendApplication {
 
 	public static void main(String[] args) {
